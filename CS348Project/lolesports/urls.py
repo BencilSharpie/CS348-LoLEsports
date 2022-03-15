b7e8cfde799b062b9b8e1931e5cca594725a47be
Melanie Molen <mmolen@purdue.edu>
2022-03-14 22:55:07 -0400

    setup views and templates for pages

---
+++ CS348Project/lolesports/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home),
+    path('schedule', views.schedule),
+    path('team', views.team),
+    path('player', views.player),
+    path('match', views.match),
+]
